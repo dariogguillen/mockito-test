b0d99bfba31f2620f38d654c84f0958aca3d8187
dariogguillen <guillendario@gmail.com>
2025-03-19 13:15:22 -0600

    test: using mockito instead ExamRepositoryImpl

--- src/test/java/org/dgg/mockito/services/ExamServiceImplTest.java
+++ src/test/java/org/dgg/mockito/services/ExamServiceImplTest.java
@@ -2,16 +2,25 @@ package org.dgg.mockito.services;
 
 import org.dgg.mockito.models.Exam;
 import org.dgg.mockito.repositories.ExamRepository;
-import org.dgg.mockito.repositories.ExamRepositoryImpl;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ExamServiceImplTest {
 
     @Test
     void findExamByName() {
-        ExamRepository examRepository = new ExamRepositoryImpl();
+        ExamRepository examRepository = mock(ExamRepository.class);
+        when(examRepository.findAll()).thenReturn(Arrays.asList(
+                new Exam(1L, "Java"),
+                new Exam(2L, "Spring"),
+                new Exam(3L, "Hibernate"),
+                new Exam(4L, "Mockito"),
+                new Exam(5L, "JUnit")
+        ));
         ExamService examService = new ExamServiceImpl(examRepository);
         Exam exam = examService.findExamByName("Mockito");
         assertNotNull(exam);
