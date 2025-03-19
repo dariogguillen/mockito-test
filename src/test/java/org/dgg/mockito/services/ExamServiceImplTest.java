ef862c629ca609fda81e8c8084900a19a6a26e28
dariogguillen <guillendario@gmail.com>
2025-03-19 13:30:56 -0600

    test: not found and empty list

--- src/test/java/org/dgg/mockito/services/ExamServiceImplTest.java
+++ src/test/java/org/dgg/mockito/services/ExamServiceImplTest.java
@@ -2,19 +2,28 @@ package org.dgg.mockito.services;
 
 import org.dgg.mockito.models.Exam;
 import org.dgg.mockito.repositories.ExamRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class ExamServiceImplTest {
+    ExamRepository examRepository;
+    ExamService examService;
+
+    @BeforeEach
+    void setUp() {
+        examRepository = mock(ExamRepository.class);
+        examService = new ExamServiceImpl(examRepository);
+    }
 
     @Test
     void findExamByName() {
-        ExamRepository examRepository = mock(ExamRepository.class);
         when(examRepository.findAll()).thenReturn(Arrays.asList(
                 new Exam(1L, "Java"),
                 new Exam(2L, "Spring"),
@@ -22,10 +31,31 @@ class ExamServiceImplTest {
                 new Exam(4L, "Mockito"),
                 new Exam(5L, "JUnit")
         ));
-        ExamService examService = new ExamServiceImpl(examRepository);
         Optional<Exam> exam = examService.findExamByName("Mockito");
         assertTrue(exam.isPresent());
         assertEquals("Mockito", exam.orElseThrow().getName());
         assertEquals(4L, exam.get().getId());
+    }
+
+    @Test
+    void findExamByName_notFound() {
+        when(examRepository.findAll()).thenReturn(Arrays.asList(
+                new Exam(1L, "Java"),
+                new Exam(2L, "Spring"),
+                new Exam(3L, "Hibernate"),
+                new Exam(4L, "Mockito"),
+                new Exam(5L, "JUnit")
+        ));
+        Optional<Exam> exam = examService.findExamByName("C++");
+        assertTrue(exam.isEmpty());
+    }
+
+    @Test
+    void findExamByName_emptyList() {
+        examRepository = mock(ExamRepository.class);
+        when(examRepository.findAll()).thenReturn(List.of());
+        examService = new ExamServiceImpl(examRepository);
+        Optional<Exam> exam = examService.findExamByName("Java");
+        assertTrue(exam.isEmpty());
     }
 }
